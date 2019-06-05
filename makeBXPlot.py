4027895509f9a56c48924f33b34bc00243b99868
Paul Lujan <paul.lujan@cern.ch>
2019-06-05 12:44:56 +0200

    spruce up makeBXPlot.py a bit and add 2018 bx plots

--- makeBXPlot.py
+++ makeBXPlot.py
@@ -5,27 +5,39 @@
 # distribution. To use it, first create a brilcalc output file using something like this:
 # brilcalc lumi -u hz/ub --byls --xing -i "{300777:[[40,64]]}" --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_PHYSICS.json -o sample.csv
 # and then run providing that file as the input.
+#
+# Arguments for this script:
+# input file -- output file from brilcalc as above
+# -n NUM -- use only the first NUM lumi sections from the input file (instead of all)
+# -o NAME -- use NAME as the name of the output file (default is bxlumi_FILL.png)
 
-import sys
-import csv
+import sys, csv, math, argparse
 import ROOT as r
-import math
 
-if (len(sys.argv) < 2):
-    print "Usage: "+sys.argv[0]+" <input file>"
-    sys.exit(1)
-
-infile = sys.argv[1]
+parser = argparse.ArgumentParser(description="Plot the distribution of BX lumi given a brilcalc output file.")
+parser.add_argument("inputFile", help="Input file from brilcalc")
+parser.add_argument("-n", "--num-ls", help="Number of lumi sections to use from file (default: all)", type=int)
+parser.add_argument("-o", "--output-file", help="Output file name (default: bxlumi_FILL.png")
+args = parser.parse_args()
 
 tot_ls = 0
 tot_lum_bx = [0] * 3565   # this goes 1...3564 so we need 3565 elements
 
-with open(infile) as csv_input:
+with open(args.inputFile) as csv_input:
     reader = csv.reader(csv_input, delimiter=',')
 
     for row in reader:
         if row[0][0] == '#':
             continue
+
+        if tot_ls == 0:
+            fill = (row[0].split(":"))[1]
+        else:
+            this_fill = (row[0].split(":"))[1]
+            if fill != this_fill:
+                print "Error: fill number changed from "+fill+" to "+this_fill+", aborting!"
+                print "Suggest using -n to limit the number of lumi sections used."
+                sys.exit(1)
 
         lsdata = row[9]
         lsdata = lsdata[1:-1]  # strip starting and ending brackets
@@ -38,6 +50,8 @@ with open(infile) as csv_input:
             tot_lum_bx[bx] += lum_deliv
 
         tot_ls += 1
+        if (args.num_ls and tot_ls >= args.num_ls):
+            break
 
 print "Read",tot_ls,"lumisections"
 
@@ -46,7 +60,7 @@ h1 = r.TH1F("h1", "h1", 70, 3, 10)
 h1.GetXaxis().SetTitle("Instantaneous luminosity (Hz/#mub)")
 h1.GetYaxis().SetTitle("Number of bunches")
 h1.GetYaxis().SetTitleOffset(1.5)
-h1.SetTitle("Luminosity per bunch")
+h1.SetTitle("Luminosity per bunch, fill "+fill)
 
 for i in range(0, 3565):
     avg_lum = tot_lum_bx[i]/tot_ls
@@ -74,6 +88,9 @@ hc.Draw()
 f1.Draw("same")
 r.gPad.SetLogy()
 hc.GetYaxis().SetRangeUser(1e-4,500)
-c1.Print("bxlumi.png")      
+if args.output_file:
+    c1.Print(args.output_file)
+else:
+    c1.Print("bxlumi_"+fill+".png")
 
 raw_input("Press ENTER to exit...")
