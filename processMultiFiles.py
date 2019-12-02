e0dff853c4d3b140ec5a2f067d7b9db1e92fb83d
Paul Lujan <paul.lujan@cern.ch>
2019-12-02 16:02:04 +0100

    add some options to processMultiFiles.py

--- processMultiFiles.py
+++ processMultiFiles.py
@@ -3,9 +3,28 @@
 # This script takes the split JSON files produced by divideJSON.py and runs brilcalc over them. As currently
 # written it does things one by one but it could also be parallelized if necessary.
 
-import os
-json_template = "split_file_%d.json"
-outfile_template = "brilcalc_lumi_%d.csv"
+import sys, os, argparse
+default_json_template = "split_file_%d.json"
+default_outfile_template = "brilcalc_lumi_%d.csv"
+default_normtag = "/cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_PHYSICS.json"
+
+parser = argparse.ArgumentParser(description="Invoke brilcalc on a series of input JSON files", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-i", "--input-file-template", help="Input JSON file name template", default=default_json_template)
+parser.add_argument("-o", "--output-file-template", help="Output file name template", default=default_outfile_template)
+parser.add_argument("-n", "--normtag", help="Normtag file to use", default=default_normtag)
+args = parser.parse_args()
+
+json_template = args.input_file_template
+outfile_template = args.output_file_template
+normtag = args.normtag
+
+if json_template.find("%d") < 0:
+    print "Error: input file name template should include %d (example: "+default_json_template+")"
+    sys.exit(1)
+
+if outfile_template.find("%d") < 0:
+    print "Error: output file name template should include %d (example: "+default_outfile_template+")"
+    sys.exit(1)
 
 i = 1
 while (1):
@@ -16,5 +35,5 @@ while (1):
     print "Processing json file",i,"please wait..."
     outfile_name = outfile_template % (i)
     if not os.path.exists(outfile_name):
-        os.system('brilcalc lumi --xing -b "STABLE BEAMS" -u hz/ub -i '+json_name+' --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_PHYSICS.json --xingTr 0.1 -o '+outfile_name)
+        os.system('brilcalc lumi --xing -b "STABLE BEAMS" -u hz/ub -i '+json_name+' --normtag '+normtag+' --xingTr 0.1 -o '+outfile_name)
     i += 1
